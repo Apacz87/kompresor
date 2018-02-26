d2c02591994941f8d66c4abc03311137f1375bf1
Tomasz Leman <11136588+Apacz87@users.noreply.github.com>
2018-02-26 16:06:14 +0100

    Update archiver.hpp

--- source/archiver.hpp
+++ source/archiver.hpp
@@ -10,18 +10,77 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-//#include <stdio.h>
+
+#include "zip.hpp"
 
 namespace archiver
 {
   enum class ArchiveType {ZIP};
   enum class CompressionAlgorithm {BZIP2, LZMA, DEFLATE, DCL, IMPLODE, WAVPACK};
 
-  bool IsFile(const std::string& t_path)
+  bool IsArchive(int t_file_descriptor)
   {
-    struct stat path_stat;
-    stat(t_path.c_str(), &path_stat);
-    return S_ISREG(path_stat.st_mode);
+    struct stat file_stat;
+    if (fstat(t_file_descriptor, &file_stat))
+    {
+      throw std::runtime_error("Reading file stat failed!");
+    }
+
+    if (S_ISREG (file_stat.st_mode))
+    {
+      // TODO: Check file size:
+      // min 22 bytes, max  4,294,967,295 bytes (4 GiB minus 1 byte) for standard ZIP, and 18,446,744,073,709,551,615 bytes (16 EiB minus 1 byte) for ZIP64
+      if (file_stat.st_size < 22)
+      {
+        return false;
+      }
+
+      int first_four_bytes;
+      read(t_file_descriptor, &first_four_bytes, 4);
+      return first_four_bytes == 0x04034b50;
+    }
+
+    return false;
+  }
+
+  void MapZipIntoMemory(int fd)
+  {
+    struct stat file_stat;
+    if (fstat(fd, &file_stat))
+    {
+      throw std::runtime_error("Reading file stat failed!");
+    }
+
+    char *p = (char*) mmap(0, file_stat.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (p == MAP_FAILED)
+    {
+      throw std::runtime_error("mmap file failed!");
+    }
+
+    zip::LocalFileHeader* first_header = (zip::LocalFileHeader*)p;
+    std::cout << "m_signature:" << first_header->m_signature << '\n';
+    std::cout << "m_version:" << first_header->m_version << '\n';
+    std::cout << "m_bit_flag:" << first_header->m_bit_flag << '\n';
+    std::cout << "m_compression_method:" << first_header->m_compression_method << '\n';
+    std::cout << "m_last_mod_time:" << first_header->m_last_mod_time << '\n';
+    std::cout << "m_last_mod_date:" << first_header->m_last_mod_date << '\n';
+    std::cout << "m_crc:" << first_header->m_crc << '\n';
+    std::cout << "m_compressed_size:" << first_header->m_compressed_size << '\n';
+    std::cout << "m_uncompressed_size:" << first_header->m_uncompressed_size << '\n';
+    std::cout << "m_file_name_lenght:" << first_header->m_file_name_lenght << '\n';
+    std::cout << "m_extra_field_length:" << first_header->m_extra_field_length << '\n';
+
+    for ( auto len = 0; len < file_stat.st_size; len++)
+    {
+      putchar(p[len]);
+    }
+
+    std::cout << '\n';
+
+    if (munmap(p, file_stat.st_size) == -1)
+    {
+      throw std::runtime_error("munmap file failed!");
+    }
   }
 
   void FileInfo(const std::string& t_file_path)
@@ -36,8 +95,9 @@ namespace archiver
       throw std::runtime_error("stat error!");
     }
 
-    std::cout << "File: " << t_file_path << '\n';
-    std::cout << "Size: " << sb.st_size << " bytes, Type: ";
+    std::cout << "--------------------------------------------------------------" << '\n';
+    std::cout << "FILE: " << t_file_path << '\n';
+    std::cout << "SIZE: " << sb.st_size << " bytes, TYPE: ";
     switch (sb.st_mode & S_IFMT)
     {
       case S_IFBLK:
@@ -72,38 +132,16 @@ namespace archiver
       throw std::runtime_error("open file failed!");
     }
 
-    if (S_ISREG (sb.st_mode))
+    if (IsArchive(fd))
     {
-      char *p = (char*) mmap(0, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
-      if (p == MAP_FAILED)
-      {
-        throw std::runtime_error("mmap file failed!");
-      }
-
-      if (close(fd) == -1)
-      {
-        throw std::runtime_error("close failed!");
-      }
-
-      int zip_signature = 0x04034b50;
-      int *first_word = (int*)p;
-
-      if (*first_word == zip_signature)
-      {
-        std::cout << "This file is a ZIP archive!\n";
-      }
-
-      for ( auto len = 0; len < sb.st_size; len++)
-      {
-        putchar(p[len]);
-      }
-
-      if (munmap(p, sb.st_size) == -1)
-      {
-        throw std::runtime_error("munmap file failed!");
-      }
-
+      MapZipIntoMemory(fd);
     }
+
+    if (close(fd) == -1)
+    {
+      throw std::runtime_error("close failed!");
+    }
+
   }
 
   class ArchiveFile
