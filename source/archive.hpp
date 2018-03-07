627ff041cf478b1b00460fea2205e8892db5d3ad
Tomasz Leman <11136588+Apacz87@users.noreply.github.com>
2018-03-07 15:11:26 +0100

    Update archive.hpp

--- source/archive.hpp
+++ source/archive.hpp
@@ -3,19 +3,22 @@
 
 #include <string>
 
-namespace amt::base
+namespace archive_management_tools::archives
 {
-  enum class ArchiveType {ZIP, GNUGZIP};
+
+  enum class ArchiveType {UNKNOWN, ZIP, GNUGZIP};
   enum class CompressionAlgorithm {BZIP2, LZMA, DEFLATE, DCL, IMPLODE, WAVPACK};
-  
+
   class Archive
   {
+    int m_file_descriptor;
+
     // TODO: Add archive file class content.
   public:
     virtual void Pack(std::string) = 0;
     virtual void Unpack(std::string) = 0;
     virtual void Save(std::string) = 0;
   };
-} // namespace amt::base
+} // namespace archive_management_tools::archives
 
 #endif
