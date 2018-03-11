9250d2bb34d8adab6db8097f47a995e136680720
Tomasz Leman <apacz87@gmail.com>
2018-03-11 13:25:58 +0100

    Update zip.cpp - zip headers in constructor

--- source/zip.cpp
+++ source/zip.cpp
@@ -1,11 +1,12 @@
 #include "zip.hpp"
+
 namespace archive_management_tools::archives::zip
 {
   void* ZipArchive::GetEndOfCentralDirectoryOffset()
   {
     unsigned int signature = 0x06054b50;
-    char* ptr = (char*)this->t_data_pointer;
-    for (size_t i = this->t_data_block_size - 176; i >=0; --i)
+    char* ptr = (char*)this->m_data_pointer;
+    for (size_t i = this->m_data_size - 176; i >=0; --i)
     {
       int* value = (int*) ptr;
       if (*value == signature)
@@ -23,23 +24,30 @@ namespace archive_management_tools::archives::zip
   {
     throw std::runtime_error("The function has not yet been implemented");
   }
+
   void ZipArchive::Unpack(std::string  t_input)
   {
     throw std::runtime_error("The function has not yet been implemented");
   }
+
   void ZipArchive::Save(std::string t_input)
   {
     throw std::runtime_error("The function has not yet been implemented");
   }
+
   ZipArchive::ZipArchive(void* t_data_pointer, size_t t_data_block_size) : m_data_pointer(t_data_pointer), m_data_size(t_data_block_size)
   {
     this->m_end_central_directory = (components::EndOfCentralDirectory*) this->GetEndOfCentralDirectoryOffset();
-    this->m_central_directory = (components::CentralDirectory*) ((char*)this->m_data_pointer) + this->m_end_central_directory->m_start_offset;
+    std::cout << "Data offset: " << this->m_data_pointer << '\n';
+    std::cout << "Offset: " << this->m_end_central_directory->m_start_offset << '\n';
+    char* pointer = reinterpret_cast<char*>(this->m_data_pointer);
+    pointer += this->m_end_central_directory->m_start_offset;
+    this->m_central_directory = (components::CentralDirectory*) pointer;
   }
 
-  ZipArchive::Print()
+  void ZipArchive::Print()
   {
     this->m_end_central_directory->print_data();
-    this->m_central_directory-print_data();
+    this->m_central_directory->print_data();
   }
 }
