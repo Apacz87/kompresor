8d28453d6744930a08a9cc26e451404bcec289e5
Tomasz Leman <apacz87@gmail.com>
2018-03-18 21:49:23 +0100

    Enable generating additional std::shared_ptr instances from Archive class

--- source/archive.hpp
+++ source/archive.hpp
@@ -2,6 +2,7 @@
 #define ARCHIVE_HPP
 
 #include <string>
+#include <memory>
 
 namespace archive_management_tools::archives
 {
@@ -9,12 +10,17 @@ namespace archive_management_tools::archives
   enum class ArchiveType {UNKNOWN, ZIP, GNUGZIP};
   enum class CompressionAlgorithm {BZIP2, LZMA, DEFLATE, DCL, IMPLODE, WAVPACK};
 
-  class Archive
+  class Archive : public std::enable_shared_from_this<Archive>
   {
     int m_file_descriptor;
 
     // TODO: Add archive file class content.
   public:
+    std::shared_ptr<Archive> GetPointer()
+    {
+        return shared_from_this();
+    }
+    virtual void PrintFileStat() = 0;
     virtual void Pack(std::string) = 0;
     virtual void Unpack(std::string) = 0;
     virtual void Save(std::string) = 0;
